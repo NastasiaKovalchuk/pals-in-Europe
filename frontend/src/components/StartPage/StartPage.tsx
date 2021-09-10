d1fd6472786b8cb019d1ff330801b4624b49bd83
unknown <denis.obraztsov.92@mail.ru>
2021-09-10 11:14:48 +0300

    Slider component

--- frontend/src/components/StartPage/StartPage.tsx
+++ frontend/src/components/StartPage/StartPage.tsx
@@ -4,6 +4,7 @@ import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./StartPage.scss";
 import { RootStateValue } from "../redux/reducers/rootReducer";
+import Slider from "./Slider/Slider";
 
 const StartPage = () => {
   const [search, setSearch] = useState("");
@@ -63,47 +64,7 @@ const StartPage = () => {
 
   return (
     <div className="d-flex flex-column align-items-center mainDiv">
-      <div
-        id="carouselExampleFade"
-        className="carousel slide carousel-fade"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src="img/1.jpg" className="d-block w-100" alt="ss" />
-          </div>
-          <div className="carousel-item">
-            <img src="img/2.jpg" className="d-block w-100" alt="ss" />
-          </div>
-          <div className="carousel-item">
-            <img src="img/1.jpg" className="d-block w-100" alt="ss" />
-          </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Предыдущий</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Следующий</span>
-        </button>
-      </div>
+      <Slider />
       <form onSubmit={sumbitHandler} className="d-flex justify-content-center">
         <input
           onChange={(e) => chooseCategory(e.target.value)}
@@ -120,10 +81,10 @@ const StartPage = () => {
       {/* @ts-ignore */}
       {filterCategories && setShow
         ? filterCategories.map((el, index) => (
-            <div key={index} onClick={(e) => getTheRightSearch(e, el)}>
-              {el}
-            </div>
-          ))
+          <div key={index} onClick={(e) => getTheRightSearch(e, el)}>
+            {el}
+          </div>
+        ))
         : ""}
       {noCategories ? <div>We don't have such a category</div> : ""}
     </div>
