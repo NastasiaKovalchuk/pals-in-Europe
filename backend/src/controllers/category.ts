f8ae16a587d56b86f20d17e71280feaa97a32eec
Elina <khisamutdinova.elina@gmail.com>
2021-09-13 23:24:00 +0200

    Added reviews

--- backend/src/controllers/category.ts
+++ backend/src/controllers/category.ts
@@ -6,8 +6,7 @@ import LocationModel from "../db/models/location.model";
 export const getAllCategoriesFromSaga: RequestHandler = async (req, res) => {
   try {
     const categoriesFind1 = await categoryModel.find();
-    const categoriesFind = categoriesFind1.map(el => el.category)
-    
+    const categoriesFind = categoriesFind1.map(el => el.category)    
     res.status(200).json({ categoriesFind });
   } catch (error) {
     console.log(error);
