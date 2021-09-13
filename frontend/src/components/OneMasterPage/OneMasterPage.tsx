672463246c9bbb6262fff3450fd16e32fcf8a258
Elina <khisamutdinova.elina@gmail.com>
2021-09-13 10:43:12 +0200

    Added placemark for each master

--- frontend/src/components/OneMasterPage/OneMasterPage.tsx
+++ frontend/src/components/OneMasterPage/OneMasterPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootStateValue } from "../redux/reducers/rootReducer";
 import { YMaps, Map, Placemark } from "react-yandex-maps";
@@ -10,27 +10,25 @@ type IdParams = {
 };
 
 export const OneMasterPage = () => {
-  const [oneMasterObj, setOneMasterObj] = useState<Master>()
+  const [oneMasterObj, setOneMasterObj] = useState<Master>();
   const { id } = useParams<IdParams>();
-  
-  const selectorMasters = useSelector(
-    (state: RootStateValue) => state.masters
-  );
-  
+
+  const selectorMasters = useSelector((state: RootStateValue) => state.masters);
+
   useEffect(() => {
     if (selectorMasters) {
-      const master = selectorMasters.filter((el) => el._id === id)
+      const master = selectorMasters.filter((el) => el._id === id);
       setOneMasterObj(master[0]);
     }
-  }, [id, oneMasterObj, selectorMasters])
+  }, [id, oneMasterObj, selectorMasters]);
 
   return (
     <>
       <div className="idMasterDiv">
         <div className="idHead">
-          <img src={oneMasterObj ? oneMasterObj.picture : ''} alt=""/>
+          <img src={oneMasterObj ? oneMasterObj.picture : ""} alt="" />
           <div className="nameAndJob">
-            <div className="name">{oneMasterObj ? oneMasterObj.name : ''}</div>
+            <div className="name">{oneMasterObj ? oneMasterObj.name : ""}</div>
             {/* <div className="job">Master {oneMasterObj.category.category}</div> */}
           </div>
           <button className="btn contactBtn" type="submit">
@@ -41,21 +39,38 @@ export const OneMasterPage = () => {
         <div className="idBody">
           <div className="leftSide">
             <div className="info">INFORMATION</div>
-            <div className="description">{oneMasterObj ? oneMasterObj.description : ''}</div>
+            <div className="description">
+              {oneMasterObj ? oneMasterObj.description : ""}
+            </div>
           </div>
           <div className="location">
             <div className="ymaps">
               <YMaps>
-                <Map
-                  defaultState={{ center: [49.75, 14.57], zoom: 4 }}
-                  className="map"
-                >
-                
-                </Map>
+                {oneMasterObj ? (
+                  <Map
+                    defaultState={{
+                      center: [
+                        oneMasterObj.location.coordinates[1],
+                        oneMasterObj.location.coordinates[0],
+                      ],
+                      zoom: 8,
+                    }}
+                    className="map"
+                  >
+                    <Placemark
+                      geometry={[
+                        oneMasterObj.location.coordinates[1],
+                        oneMasterObj.location.coordinates[0],
+                      ]}
+                    />
+                  </Map>
+                ) : (
+                  ""
+                )}
               </YMaps>
             </div>
             <div className="links">
-              <div>City: {oneMasterObj ? oneMasterObj.location.city : ''}</div>
+              <div>City: {oneMasterObj ? oneMasterObj.location.city : ""}</div>
               <hr className="dropdown-divider" />
               <div>CONTACTS:</div>
               <div>sadadad</div>
@@ -64,7 +79,7 @@ export const OneMasterPage = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default React.memo(OneMasterPage);
